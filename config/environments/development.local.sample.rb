2f76bd7832e06e13521506570c3cb7b2aa66850a
Aiandrox <aiandrox7@gmail.com>
2021-03-06 02:06:22 +0900

    [Add] 個人ごとに違う設定は/config/environments/development.local.rbに記述する

---
+++ config/environments/development.local.sample.rb
@@ -0,0 +1,10 @@
+Rails.application.configure do
+  # bulletの設定
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+    Bullet.add_footer = true
+  end
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,13 +73,10 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+end
 
-  # bulletの設定
-  config.after_initialize do
-    Bullet.enable = true
-    Bullet.bullet_logger = true
-    Bullet.console = true
-    Bullet.rails_logger = true
-    Bullet.add_footer = true
-  end
+if File.exist?(Rails.root.join('config/environments/development.local.rb'))
+  require_relative 'development.local'
+else
+  require_relative 'development.local.sample'
 end
