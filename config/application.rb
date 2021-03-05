0497c4202663bb4022da8ad6c12533013a32a7c4
Aiandrox <aiandrox7@gmail.com>
2021-03-06 01:31:19 +0900

    [Lint] Rubocopの指摘修正 https://qiita.com/jnchito/items/418277508dce263cf4f0

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module RunteqSenses
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.i18n.default_locale = :ja
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
   end
