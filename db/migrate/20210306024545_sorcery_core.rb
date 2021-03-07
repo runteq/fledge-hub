e79907336e7f472a6b3099e18f05a1bc5134dd70
Aiandrox <aiandrox7@gmail.com>
2021-03-08 00:07:52 +0900

    [Add] Userモデルを追加

--- db/migrate/20210306024545_sorcery_core.rb
+++ db/migrate/20210306024545_sorcery_core.rb
@@ -1,13 +1,16 @@
 class SorceryCore < ActiveRecord::Migration[6.1]
   def change
-    create_table :User do |t|
-      t.string :email,            null: false
+    create_table :users do |t|
+      t.string :display_name, null: false
+      t.string :screen_name, null: false
+      t.string :email, null: false
       t.string :crypted_password
       t.string :salt
 
-      t.timestamps                null: false
+      t.timestamps null: false
     end
 
-    add_index :User, :email, unique: true
+    add_index :users, :email, unique: true
+    add_index :users, :screen_name, unique: true
   end
 end
