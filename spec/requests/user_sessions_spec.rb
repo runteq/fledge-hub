6ac50b584c46b86f1c6caf0b1c7484ca80b8fc49
Aiandrox <aiandrox7@gmail.com>
2021-03-14 15:38:22 +0900

    [Pend]ログイン周りのスペックをコメントアウト

---
+++ spec/requests/user_sessions_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "UserSessions", type: :request do
+  # describe "DELETE /logout" do
+  #   before { login }
+  #   subject { delete '/logout' }
+  #   it "returns http success" do
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+end
