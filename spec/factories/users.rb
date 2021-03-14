43dcc0b3ef8cc45dc8d1ebe0345b0e516926a6c6
Aiandrox <aiandrox7@gmail.com>
2021-03-14 17:32:38 +0900

    [Fix] カラム名を間違えていたので修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    sequence(:name, "名前_1")
+    sequence(:display_name, "名前_1")
     sequence(:screen_name, "screen_name_1")
     sequence(:email){|n| "test_#{n}@example.com"}
   end
