c9fa632a64e3faf20e71b6ecbdf9eae6e2442ddc
Aiandrox <aiandrox7@gmail.com>
2021-03-17 23:33:09 +0900

    [Add] 開発者ログインURLを追加

---
+++ app/controllers/development/sessions_controller.rb
@@ -0,0 +1,7 @@
+class Development::SessionsController < ActionController::Base
+  def login_as
+    user = User.find(params[:user_id])
+    auto_login(user)
+    redirect_to root_path, notice: "#{Rails.env}環境でログインしました"
+  end
+end
