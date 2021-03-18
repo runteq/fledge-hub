f5a1655cb709aece3d4ee91f290e03bc2e126cb7
Aiandrox <aiandrox7@gmail.com>
2021-03-18 09:49:44 +0900

    [Add] バリデーションを追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,9 @@ class Product < ApplicationRecord
   has_many :user_products, dependent: :destroy
   has_many :users, through: :user_products
 
+  validates :title, presence: true
+  validates :released_on, presence: true
+
   def permitted_edit?(user)
     !!user&.in?(users)
   end
