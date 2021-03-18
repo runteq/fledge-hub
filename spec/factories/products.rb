d17e04c1054b407aa70b07c87031be977d017d78
Aiandrox <aiandrox7@gmail.com>
2021-03-18 21:39:51 +0900

    [Add] annotateを追加

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :bigint           not null, primary key
+#  description :text(65535)
+#  released_on :date             not null
+#  sourse_url  :string(255)
+#  title       :string(255)      not null
+#  url         :string(255)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 FactoryBot.define do
   factory :product do
     sequence(:title, "プロダクト_1")
--- spec/models/user_product_spec.rb
+++ spec/models/user_product_spec.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: user_products
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  product_id :bigint           not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_user_products_on_product_id  (product_id)
+#  index_user_products_on_user_id     (user_id)
+#
 require 'rails_helper'
 
 RSpec.describe UserProduct, type: :model do
