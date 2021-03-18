bf2e2f2a381c8916b13a905cb9bf9130dd3d8dd6
Aiandrox <aiandrox7@gmail.com>
2021-03-18 09:15:12 +0900

    [Add] 自分によるproductが判定するメソッドを追加 current_userがnilのときもあるので、productにメソッドを生やした

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
   has_many :user_products, dependent: :destroy
   has_many :users, through: :user_products
+
+  def permitted_edit?(user)
+    !!user&.in?(users)
+  end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#permitted_edit?' do
+    let(:product) { create(:product) }
+    subject { product.permitted_edit?(user) }
+
+    context '自分によるproductではないとき' do
+      let(:user) { create(:user) }
+      it { is_expected.to eq false }
+    end
+
+    context '自分のproductのとき' do
+      let(:user) { create(:user, products: [product]) }
+      it { is_expected.to eq true }
+    end
+
+    context 'userがnilのとき' do
+      let(:user) { nil }
+      it { is_expected.to eq false }
+    end
+  end
 end
