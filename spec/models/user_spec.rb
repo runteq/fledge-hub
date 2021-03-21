89a3889406b3c3e5768185866a8ca8cc245d6c1b
Aiandrox <aiandrox7@gmail.com>
2021-03-22 01:32:16 +0900

    [Add] User#deactivate!のテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,4 +20,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe '#deactivate!' do
+    let(:user) { create(:user, id: 1, status: :general) }
+    subject { user.deactivate! }
+    it { expect { subject }.to change { user.reload.display_name }.to('removed_account_1') }
+    it { expect { subject }.to change { user.reload.screen_name }.to('removed_account_1') }
+    it { expect { subject }.to change { user.reload.email }.to('removed_account_1@example.com') }
+    it { expect { subject }.to change { user.reload.status }.to('deactivated').from('general') }
+  end
 end
