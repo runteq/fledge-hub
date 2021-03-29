fae7566d9cde495c6223bd34f747590cd38e28f9
Aiandrox <aiandrox7@gmail.com>
2021-03-29 23:18:41 +0900

    [Add] Mediumモデルを追加

---
+++ db/migrate/20210329123133_create_media.rb
@@ -0,0 +1,12 @@
+class CreateMedia < ActiveRecord::Migration[6.1]
+  def change
+    create_table :media do |t|
+      t.string :title, null: false
+      t.text :description
+      t.text :url, null: false
+      t.references :product, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_005251) do
+ActiveRecord::Schema.define(version: 2021_03_29_123133) do
 
   create_table "authentications", charset: "utf8mb4", collation: "utf8mb4_bin", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -30,6 +30,16 @@ ActiveRecord::Schema.define(version: 2021_03_22_005251) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["product_id"], name: "index_images_on_product_id"
+  end
+
+  create_table "media", charset: "utf8mb4", collation: "utf8mb4_bin", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description"
+    t.text "url", null: false
+    t.bigint "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_media_on_product_id"
   end
 
   create_table "products", charset: "utf8mb4", collation: "utf8mb4_bin", force: :cascade do |t|
@@ -65,4 +75,5 @@ ActiveRecord::Schema.define(version: 2021_03_22_005251) do
   end
 
   add_foreign_key "images", "products"
+  add_foreign_key "media", "products"
 end
