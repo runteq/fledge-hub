b806d2e231d8c4ecc15376b409b9b1a4b12530ad
Aiandrox <aiandrox7@gmail.com>
2021-04-11 19:02:57 +0900

    [Fix] -は正規表現のエスケープまたは最初か最後に書く必要があるので修正

--- app/models/technology.rb
+++ app/models/technology.rb
@@ -18,5 +18,5 @@ class Technology < ApplicationRecord
   has_many :products, through: :product_technologies
 
   validates :name, uniqueness: true
-  validates :slug, uniqueness: true, format: { with: /\A[a-zA-Z0-9-_]+\z/ }
+  validates :slug, uniqueness: true, format: { with: /\A[a-zA-Z0-9_-]+\z/, message: "この%{attribute}%{value}はだめです" }
 end
