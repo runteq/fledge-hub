4a5eaa6762589619a73c0f9387ba51822e126675
watsumi <w.utsumi2@gmail.com>
2021-04-28 11:33:08 +0900

    [Add]ヘルパーメソッド追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,10 @@ module ApplicationHelper
     URI.parse(url)
     link_to text, sanitize(url), target: :_blank, rel: 'noopener noreferrer'
   end
+
+  def page_title(page_title = '')
+    base_title = 'RUNTEQ senses'
+
+    page_title.empty? ? base_title : page_title + ' - ' + base_title
+  end
 end
