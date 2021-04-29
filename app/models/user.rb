aecb3bc82a563e51a7d1594bcc5049ba9e43ffa6
watsumi <w.utsumi2@gmail.com>
2021-04-30 07:36:21 +0900

    [Remove]display_nameのバリデーションを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,6 @@ class User < ApplicationRecord
   has_many :authentications, dependent: :destroy
   accepts_nested_attributes_for :authentications
 
-  validates :display_name, presence: true
   validates :screen_name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
 
