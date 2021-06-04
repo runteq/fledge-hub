e245b9b212e3b7d7fc6fe6dab28eac6aeca83d6d
Aiandrox <aiandrox7@gmail.com>
2021-06-04 23:48:17 +0900

    [Add] 退会時に削除されるデータを列挙する

--- app/controllers/user_deactivations_controller.rb
+++ app/controllers/user_deactivations_controller.rb
@@ -1,9 +1,12 @@
 class UserDeactivationsController < ApplicationController
-  def new; end
+  def new
+    @deactivated_product_sample = current_user.products.first.clone
+    @deactivated_product_sample.user.display_name = '退会済みユーザー'
+  end
 
   def destroy
     current_user.deactivate!
     logout
-    redirect_to users_url, notice: '退会しました'
+    redirect_to users_url, notice: '退会処理が完了しました。ありがとうございました。'
   end
 end
