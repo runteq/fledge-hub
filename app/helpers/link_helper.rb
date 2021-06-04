9e5d80a2a3dd9e888d487457fe4ee1e531e0525f
Aiandrox <aiandrox7@gmail.com>
2021-06-05 01:15:08 +0900

    [Add] 退会済みユーザーのときはユーザーページへのリンクを表示しないようにする

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -3,4 +3,8 @@ module LinkHelper
     URI.parse(url)
     link_to text, sanitize(url), target: :_blank, rel: 'noopener noreferrer'
   end
+
+  def link_to_active(user)
+    user.deactivated? ? user.display_name : link_to(user.display_name, user_path(user.screen_name))
+  end
 end
