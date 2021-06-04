1bba2bf600bbfeb472a9368029de99e0415b7f38
Aiandrox <aiandrox7@gmail.com>
2021-06-05 01:23:56 +0900

    [Fix] テストの修正

--- spec/requests/user_deactivations_spec.rb
+++ spec/requests/user_deactivations_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe UserDeactivationsController, type: :request do
-  let!(:user) { create(:user) }
+  let!(:user) { create(:user, :active) }
   before { login_as(user) }
 
   describe "GET /new" do
