836561d0364348c17b23b97b8cea7b7cfc92dee4
Aiandrox <aiandrox7@gmail.com>
2021-06-05 17:49:14 +0900

    [Rename] その他のページを変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
   end
 
   def page_title(page_title = '')
-    base_title = 'RUNTEQ senses'
+    base_title = 'Fledge Hub'
 
     page_title.empty? ? base_title : page_title + ' - ' + base_title
   end
