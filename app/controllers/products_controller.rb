576ed8cf31d540a1ba80896430b0ba72cdd58a8c
Aiandrox <aiandrox7@gmail.com>
2021-06-06 12:23:36 +0900

    [Add] ジャンルのセレクトボックスを追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -47,6 +47,6 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :summary, :url, :source_url, :released_on, technology_ids: [])
+    params.require(:product).permit(:title, :summary, :url, :source_url, :released_on, :genre_id, technology_ids: [])
   end
 end
