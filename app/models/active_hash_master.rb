cb2489c2c6f1c4f789111fc9e37cd0c0ecad5f1f
Aiandrox <aiandrox7@gmail.com>
2021-06-06 12:31:06 +0900

    [Add] アソシエーションを追加

--- app/models/active_hash_master.rb
+++ app/models/active_hash_master.rb
@@ -1,8 +1,10 @@
 class ActiveHashMaster < ActiveYaml::Base
+  include ActiveHash::Associations
+  set_root_path Rails.root.join('db/fixtures')
+
   def self.inherited(klass)
     super
 
-    klass.set_root_path Rails.root.join('db/fixtures')
     klass.set_filename klass.to_s.underscore.singularize
   end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,3 @@
 class Genre < ActiveHashMaster
+  has_many :products
 end
