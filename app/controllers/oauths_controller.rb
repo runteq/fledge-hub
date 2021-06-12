576e9590a7c016a6d11290b816a4c04f508ccf7c
Aiandrox <aiandrox7@gmail.com>
2021-06-12 12:42:51 +0900

    [Refactor] ユーザー作成処理をprivateメソッド内に移動 @user_hashはSorceryで使う値なのでインスタンス変数のまま

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -8,17 +8,22 @@ class OauthsController < ApplicationController
     if login_from(provider)
       redirect_to root_path, notice: "#{provider.titleize}でログインしました"
     else
-      @user_hash[:user_info]['name'] ||= @user_hash[:user_info]['login']
-      url = URI.parse(@user_hash[:user_info]['avatar_url'])
-      user = create_from(provider)
-      user.grab_avatar_image(url)
-      reset_session
+      create_user_from(provider)
       auto_login(user)
       redirect_to root_path, notice: "#{provider.titleize}で新規登録しました"
     end
   end
 
   private
+
+  def create_user_from(provider)
+    # display_nameが登録されていないときはscreen_nameを使う
+    @user_hash[:user_info]['name'] ||= @user_hash[:user_info]['login']
+    url = URI.parse(@user_hash[:user_info]['avatar_url'])
+    user = create_from(provider)
+    user.grab_avatar_image(url)
+    reset_session
+  end
 
   def auth_params
     params.permit(:code, :provider)
