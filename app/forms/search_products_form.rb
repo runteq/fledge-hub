b0c8698d277822ef0822755f7ecdfa4c61e70da3
estsn122 <y.tani9411@gmail.com>
2021-06-13 23:32:41 +0900

    ひらがなとカタカナ両方で検索するよう実装

--- app/forms/search_products_form.rb
+++ app/forms/search_products_form.rb
@@ -1,11 +1,21 @@
 class SearchProductsForm
   include ActiveModel::Model
+  include KanaHelper
 
   attr_accessor :title
 
   def search
     relation = Product
-    relation = relation.where('title LIKE ?', "%#{title}%") if title.present?
+    relation = search_title(relation) if title.present?
     relation.order(created_at: :desc)
+  end
+
+  private
+
+  def search_title(relation)
+    like_hiragana = "%#{to_hiragana(title)}%"
+    like_katakana = "%#{to_katakana(title)}%"
+
+    relation.where('title LIKE ? or title LIKE ?', like_hiragana, like_katakana)
   end
 end
