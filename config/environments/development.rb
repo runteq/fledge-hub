0a1d300ce6cb040e69e52604222439e28ce60e53
Kaoru Endo <aiandrox7@gmail.com>
2021-06-18 23:29:19 +0900

    [Add] http://lvh.me:3000 でlocalhost:3000にアクセスできるようにする

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,6 +73,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << 'lvh.me'
 end
 
 if File.exist?(Rails.root.join('config/environments/development.local.rb'))
