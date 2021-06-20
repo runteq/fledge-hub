1fea07cd4f32c9d6511945f0500f7f06b43143a1
watsumi <w.utsumi2@gmail.com>
2021-06-20 21:25:52 +0900

    FormObject追加

---
+++ app/controllers/inquiries_controller.rb
@@ -0,0 +1,21 @@
+class InquiriesController < ApplicationController
+  def new
+    @inquiry_form = InquiryForm.new
+  end
+
+  def create
+    @inquiry_form = InquiryForm.new(inquiry_params)
+
+    if @inquiry_form.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def inquiry_params
+    params.require(:inquiry_form).permit(:name, :contact, :title, :description).merge(user_agent: request.user_agent)
+  end  
+end
---
+++ app/forms/inquiry_form.rb
@@ -0,0 +1,39 @@
+class InquiryForm
+  include ActiveModel::Model
+
+  attr_accessor :name, :contact, :title, :description, :user_agent
+
+  validates :name, :title, :description, presence: true
+
+  def save
+    return false if invalid?
+    post_to_mattermost
+    save_inquiry!
+    true
+  end
+
+  private
+
+  def post_to_mattermost
+    mattermost_url = 'https://chat.runteq.jp'
+  
+    conn = Faraday::Connection.new(url: mattermost_url) do |faraday|
+      faraday.request :url_encoded
+      faraday.response :raise_error
+      faraday.headers['Content-Type'] = 'application/json'
+      faraday.adapter Faraday.default_adapter
+    end
+
+    conn.post do |req|
+      req.url ENV['MATTERMOST_WEBHOOK_URL']
+      req.headers['Content-Type'] = 'application/json'
+      req.body = { 
+        text: "name: #{@name}\n contact: #{@contact}\n title: #{@title}\n description: #{@description}\n user_agent: #{@user_agent}" 
+      }.to_json
+    end
+  end
+
+  def save_inquiry!
+    Inquiry.new(name: @name, contact: @contact, title: @title, description: @description, user_agent: @user_agent).save!
+  end
+end
