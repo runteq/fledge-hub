8b9add3f88ef9629988df1099e4aa0f3197fe4ff
watsumi <w.utsumi2@gmail.com>
2021-06-26 13:19:59 +0900

    mockが効いてなかったので修正

--- spec/forms/inquiry_form_spec.rb
+++ spec/forms/inquiry_form_spec.rb
@@ -5,16 +5,15 @@ WebMock.allow_net_connect! # inquiry以外ではwebmockは使わない
 MATTERMOST_WEBHOOK_URL = Rails.application.credentials.mattermost[:webhook_url]
 
 RSpec.describe InquiryForm do
+  before do
+    WebMock.disable!(except: [:net_http])
+    WebMock.stub_request(:post, MATTERMOST_WEBHOOK_URL).to_return(
+      body: {"text": "test_inquiry_form"}.to_json,
+      status: 200,
+      headers: { 'Content-Type' =>  'application/json' }
+    )
+  end
   describe "#post_to_mattermost" do
-    before do
-      WebMock.disable!(except: [:net_http])
-      WebMock.stub_request(:post, MATTERMOST_WEBHOOK_URL).to_return(
-        body: {"text": "test_inquiry_form"}.to_json,
-        status: 200,
-        headers: { 'Content-Type' =>  'application/json' }
-      )
-    end
-
     subject { InquiryForm.new }
     it "will_be_successful" do
       response = subject.send(:post_to_mattermost)
@@ -23,7 +22,7 @@ RSpec.describe InquiryForm do
   end
 
   describe '#save' do
-    subject { form.save }
+    subject { form.send(:save) }
     let!(:form) { InquiryForm.new(**attributes) }
 
     context 'バリデーションエラー' do
