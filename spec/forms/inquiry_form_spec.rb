d856c10298fc26a169193ecb303fb97b183aff7d
watsumi <w.utsumi2@gmail.com>
2021-06-26 09:15:49 +0900

    #saveのrspec追加

--- spec/forms/inquiry_form_spec.rb
+++ spec/forms/inquiry_form_spec.rb
@@ -22,7 +22,46 @@ RSpec.describe InquiryForm do
     end
   end
 
-  # saveメソッドのテストかく
-  # describe "#save" do
-  # end
+  describe '#save' do
+    subject { form.save }
+    let!(:form) { InquiryForm.new(**attributes) }
+
+    context 'バリデーションエラー' do
+      let!(:attributes) do
+        {
+          name: '', # バリデーションエラー
+          email: 'test@example.com',
+          about: 'about_test',
+          description: '', # バリデーションエラー
+          user_agent: 'user_agent_test'
+        }
+      end
+
+      it { expect(subject).to be false }
+      it 'レコードを作成しない' do
+        expect { subject }.not_to change(Inquiry, :count)
+      end
+      it 'エラーメッセージを持つ' do
+        subject
+        expect(form.errors.messages).to eq({ name: ['を入力してください'], description: ['を入力してください']})
+      end
+    end
+
+    context '正常系' do
+      let!(:attributes) do
+        {
+          name: 'name_test',
+          email: 'test@example.com',
+          about: 'about_test',
+          description: 'description_test',
+          user_agent: 'user_agent_test'
+        }
+      end
+
+      it { expect(subject).to be true }
+      it 'レコードを作成する' do
+        expect { subject }.to change(Inquiry, :count).by(1)
+      end
+    end
+  end
 end
