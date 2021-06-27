2c213f3117c1109f8e80e5d3f8e632b058c1b1bc
watsumi <w.utsumi2@gmail.com>
2021-06-27 18:01:53 +0900

    post_to_mattermostメソッドを分割

--- app/forms/inquiry_form.rb
+++ app/forms/inquiry_form.rb
@@ -21,11 +21,15 @@ class InquiryForm
   def post_to_mattermost
     mattermost_url = 'https://chat.runteq.jp'
     mattermost_channel_url = Rails.application.credentials.mattermost[:webhook_url]
-    payload = {
+    post_payload = {
       text: "| name | #{name} |\n | -- | -- |\n | email | #{email} |\n | about | #{about}|\n | description | #{description}|\n | user_agent | #{user_agent}|"
     }
 
-    conn = Faraday::Connection.new(url: mattermost_url) do |faraday|
+    post_by_faraday(mattermost_url, mattermost_channel_url, post_payload)
+  end
+
+  def post_by_faraday(post_url, channel_url, payload)
+    conn = Faraday::Connection.new(url: post_url) do |faraday|
       faraday.request :url_encoded
       faraday.response :raise_error
       faraday.headers['Content-Type'] = 'application/json'
@@ -33,7 +37,7 @@ class InquiryForm
     end
 
     conn.post do |req|
-      req.url mattermost_channel_url
+      req.url channel_url
       req.headers['Content-Type'] = 'application/json'
       req.body = payload.to_json
     end
