72d6e4a92e06bb27e7736cb7ae0af9a1f3ae13b2
watsumi <w.utsumi2@gmail.com>
2021-07-03 22:23:58 +0900

    [Add]svgを埋め込めるようにした

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,14 @@ module ApplicationHelper
   def meta_image_url(image_url = '')
     image_url.presence || 'デフォルトのOGP用URL'
   end
+
+  def embedded_svg filename, options={}
+    file = File.read(Rails.root.join('app', 'assets', 'images', filename))
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css 'svg'
+    if options[:class].present?
+      svg['class'] = options[:class]
+    end
+    doc.to_html.html_safe
+  end
 end
