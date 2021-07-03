f7918ea892c92ccfa158c965efbf7ee62ae7f62f
Aiandrox <aiandrox7@gmail.com>
2021-07-03 13:15:05 +0900

    [Add] タブをViewComponentで作成

---
+++ app/components/settings_tab_component.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class SettingsTabComponent < ViewComponent::Base
+  def initialize(tab_name:, tab_path:)
+    @tab_name = tab_name
+    @tab_path = tab_path
+  end
+
+  private
+
+  attr_reader :tab_name, :tab_path
+end
