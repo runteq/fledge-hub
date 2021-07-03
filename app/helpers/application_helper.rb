8eb78cd77179be4ade129154a6ad8fcfed3115ef
watsumi <w.utsumi2@gmail.com>
2021-07-03 22:49:43 +0900

    [Fix]rubocopの指摘箇所を修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,13 +13,11 @@ module ApplicationHelper
     image_url.presence || 'デフォルトのOGP用URL'
   end
 
-  def embedded_svg filename, options={}
+  def embedded_svg(filename, options = {})
     file = File.read(Rails.root.join('app', 'assets', 'images', filename))
     doc = Nokogiri::HTML::DocumentFragment.parse file
     svg = doc.at_css 'svg'
-    if options[:class].present?
-      svg['class'] = options[:class]
-    end
-    doc.to_html.html_safe
+    svg['class'] = options[:class] if options[:class].present?
+    content_tag(doc.to_html.delete_prefix('<').chop)
   end
 end
