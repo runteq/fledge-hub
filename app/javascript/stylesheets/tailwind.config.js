94e1c486b5e0e54bb9188defa776b762ab0ff2c7
Yasunari_Kainuna <yasu6491@icloud.com>
2021-07-13 09:38:18 +0900

    fix:Chrome検索フォームオートコンプリートを使用時の背景色変更対応用cssをtailwindのbox shadowのカスタマイズに追加

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    boxShadow: {
+      // Chrome検索フォームオートコンプリートを使用時の背景色を変更しない様に対応
+      autocomplete: '0 0 0 1000px white inset'
+    }
   },
   variants: {
     extend: {},
