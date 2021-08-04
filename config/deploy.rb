581568ffb254f32c8c239e44dbab23cbd925449e
Shotaro Hirose <shotaro199959@icloud.com>
2021-08-04 17:41:34 +0900

    Fix: if fetch(:nginx_use_ssl) 削除

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,6 @@ set :puma_pid, '/var/www/fledge-hub/shared/tmp/pids/puma.pid'
 set :puma_access_log, '/var/www/fledge-hub/shared/log/puma.error.log'
 set :puma_error_log, '/var/www/fledge-hub/shared/log/puma.access.log'
 set :puma_preload_app, true
-set :nginx_use_ssl, true
 
 # Default branch is :main
 set :branch, ENV['BRANCH'] || 'main'
