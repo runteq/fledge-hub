1f784c40c0a9411852689d4d1f4ae47cfac0c5f2
Aiandrox <aiandrox7@gmail.com>
2021-08-09 16:52:58 +0900

    [Fix] 引数のデフォルト値が設定されているので不要

--- spec/forms/inquiry_form_spec.rb
+++ spec/forms/inquiry_form_spec.rb
@@ -38,7 +38,6 @@ RSpec.describe InquiryForm do
       it { expect(subject).to be true }
       specify do
         expect(MattermostNotifier).to receive(:call).with(
-          channel_url: MattermostNotifier::WEBHOOK_URL,
           text: "| name | name_test |\n | -- | -- |\n | email | test@example.com |\n | about | about_test|\n | description | description_test|\n | user_agent | user_agent_test|",
         )
         subject
