f101e39804cbcecf02a161c9a90a026fec44ced3
krpk1900 <krpk1900@gmail.com>
2021-08-09 15:55:57 +0900

    [Fix] 100行に収まるように改行をはさんでコードを修正

--- spec/decorators/product_decorator_spec.rb
+++ spec/decorators/product_decorator_spec.rb
@@ -7,21 +7,22 @@ RSpec.describe ProductDecorator, type: :decorator do
     context 'リリース日が昨日の場合' do
       let(:product) { create(:product, :yesterday) }
       it 'releasedと表示される' do
-        expect( decorate(product).release_day_message ).to eq "#{product.released_on} released"
+        expect(decorate(product).release_day_message).to eq "#{product.released_on} released"
       end
     end
 
     context 'リリース日が今日の場合' do
       let(:product) { create(:product, :today) }
       it 'releasedと表示される' do
-        expect( decorate(product).release_day_message ).to eq "#{product.released_on} released"
+        expect(decorate(product).release_day_message).to eq "#{product.released_on} released"
       end
     end
 
     context 'リリース日が明日の場合' do
       let(:product) { create(:product, :tomorrow) }
-      it 'will be releasedと表示される' do
-        expect( decorate(product).release_day_message ).to eq "#{product.released_on} will be released"
+      fit 'will be releasedと表示される' do
+        expect(decorate(product).release_day_message).to \
+          eq "#{product.released_on} will be released"
       end
     end
   end
