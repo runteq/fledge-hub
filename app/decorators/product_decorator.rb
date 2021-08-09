c4ad419dd1c2b07e1aa9bb475ccd305d8ad98322
krpk1900 <krpk1900@gmail.com>
2021-08-09 13:50:43 +0900

    [Fix] リリース日とメッセージの表示ロジックをmodelからdecoratorに移行

--- app/decorators/product_decorator.rb
+++ app/decorators/product_decorator.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 module ProductDecorator
+  def release_day_message
+    Time.zone.today < released_on ? "#{released_on} will be released" : "#{released_on} released"
+  end
 end
