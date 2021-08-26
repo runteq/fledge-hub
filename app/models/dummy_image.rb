0faf09b8c68b4a2b07f9307425b3000302a3b6aa
Aiandrox <aiandrox7@gmail.com>
2021-08-26 10:01:07 +0900

    [Add] 変数を入れられるようにした

--- app/models/dummy_image.rb
+++ app/models/dummy_image.rb
@@ -1,8 +1,8 @@
 class DummyImage
   def product_image
     dummy_image = Struct.new('DummyImage') do
-      def variant(resize_to_fill: [])
-        'https://placehold.jp/70/d6d6d6/7d7d7d/800x450.png?text=No%20Image'
+      def variant(resize_to_fill:)
+        "https://placehold.jp/70/d6d6d6/7d7d7d/#{resize_to_fill[0]}x#{resize_to_fill[1]}.png?text=No%20Image"
       end
     end
     dummy_image.new
