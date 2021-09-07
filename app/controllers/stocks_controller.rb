ef5b9a5805e3b8572fe40c56ffc877d2134ab2d6
Aiandrox <aiandrox7@gmail.com>
2021-09-08 00:33:41 +0900

    [Add] ストック機能を最終調整

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,10 +1,14 @@
 class StocksController < ApplicationController
+  before_action :require_login
+
   def create
-    current_user.stocks.create!(product_id: params[:product_id])
+    current_user.stocks.find_or_create_by(product_id: params[:product_id])
+    render status: :created
   end
 
   def destroy
     stock = current_user.stocks.find_by!(product_id: params[:product_id])
     stock.destroy!
+    render status: :ok
   end
 end
