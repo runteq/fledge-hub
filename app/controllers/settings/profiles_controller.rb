5352b5ba8737162b39b079b9ae7adf1c7d9c9369
ohno <ddeeppaa.gogo@gmail.com>
2021-09-23 17:37:00 +0900

    Add: プロフィール画像を更新出来るように変更。

--- app/controllers/settings/profiles_controller.rb
+++ app/controllers/settings/profiles_controller.rb
@@ -16,7 +16,7 @@ module Settings
     private
 
     def user_params
-      params.require(:user).permit(:display_name, :email)
+      params.require(:user).permit(:display_name, :email, :avatar)
     end
   end
 end
