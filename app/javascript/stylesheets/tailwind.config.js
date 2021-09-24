3808d7b664463ad1b2a9f693f6509493dd3bb895
Aiandrox <aiandrox7@gmail.com>
2021-09-25 01:35:45 +0900

    [Add] 2xsフォントサイズを追加

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -4,6 +4,19 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontSize: {
+      "2xs": "0.5rem",
+      xs: ".75rem",
+      sm: ".875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem",
+    },
     extend: {
       colors,
     },
