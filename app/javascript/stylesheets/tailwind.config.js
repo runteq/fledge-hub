1502b6fabc34a94095883024e9d0a51f21e64d19
Aiandrox <aiandrox7@gmail.com>
2021-09-25 01:18:12 +0900

    [Add] Tailwindのデフォルトカラーを追加 https://tailwindcss.com/docs/customizing-colors#color-palette-reference

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,8 +1,12 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors,
+    },
   },
   variants: {
     extend: {},
