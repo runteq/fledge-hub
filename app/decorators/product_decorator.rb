b303169fbc8261a09d20283cb7cb9ac79364a152
Aiandrox <aiandrox7@gmail.com>
2021-10-02 04:58:42 +0900

    [Add] showページのmeta_tagの設定 https://codezine.jp/article/detail/11253

--- app/decorators/product_decorator.rb
+++ app/decorators/product_decorator.rb
@@ -4,4 +4,20 @@ module ProductDecorator
   def release_day_message
     Time.zone.today < released_on ? "#{released_on} will be released" : "#{released_on} released"
   end
+
+  def to_meta_tags
+    {
+      title: title,
+      description: summary,
+      keywords: "#{product_type.name},#{product_category.name},#{technologies.pluck(:name).join(',')}",
+      og: {
+        title: :full_title,
+        image: product_thumbnail_url(top_image),
+      },
+      twitter: {
+        title: :full_title,
+        image: product_thumbnail_url(top_image),
+      },
+    }
+  end
 end
