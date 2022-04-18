3f99bdf22be0a016cd2f549642f44726d4d43968
Aiandrox <aiandrox7@gmail.com>
2022-04-19 00:42:05 +0900

    [Fix] OGP用にpublicな画像URLを使うようにする

--- app/decorators/product_decorator.rb
+++ app/decorators/product_decorator.rb
@@ -13,12 +13,18 @@ module ProductDecorator
         "#{product_type.name},#{product_category.name},#{technologies.pluck(:name).join(',')}",
       og: {
         title: :full_title,
-        image: product_thumbnail_url(top_image),
+        image: ogp_url,
       },
       twitter: {
         title: :full_title,
-        image: product_thumbnail_url(top_image),
+        image: ogp_url,
       },
     }
+  end
+
+  def ogp_url
+    return rails_representation_url(top_image.product_image.variant(resize_to_fill: [800, 450])) if top_image
+
+    ProductImage::NO_IMAGE_URL
   end
 end
