56b02d1a8be6d6d0c78b18d7ee606e11f64e49f1
Aiandrox <aiandrox7@gmail.com>
2022-11-10 05:00:19 +0900

    [Ignore] lint

--- app/javascript/controllers/recaptcha_v2_controller.js
+++ app/javascript/controllers/recaptcha_v2_controller.js
@@ -4,6 +4,7 @@ export default class extends Controller {
   static values = { siteKey: String };
 
   initialize() {
+    // eslint-disable-next-line no-undef
     grecaptcha.render("recaptchaV2", { sitekey: this.siteKeyValue });
   }
 }
