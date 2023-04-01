8fd42788746aa49fb17084872851a3a075f5ebaa
Aiandrox <aiandrox7@gmail.com>
2023-04-01 17:52:44 +0900

    [Refactor] Mattermostもオブジェクト指向っぽく記述

--- spec/models/external/mattermost_client_spec.rb
+++ spec/models/external/mattermost_client_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe External::MattermostClient do
-  describe '.post' do
+  describe '#post' do
     it 'Faraday::Connection のインスタンスにpostしていること' do
       client_double = instance_double(Faraday::Connection)
       allow(Faraday::Connection).to receive(:new).and_return(client_double)
       expect(client_double).to receive(:post)
 
-      External::MattermostClient.post({ payload: 'payload' })
+      External::MattermostClient.new.post({ payload: 'payload' })
     end
   end
 end
